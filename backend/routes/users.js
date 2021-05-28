d43a7f181645fb5f5c1756c89e244b73aa0e3f19
Vallisha M <vallishamohan@gmail.com>
2021-05-28 19:14:33 +0530

    added login and signup backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
 let User = require('../models/user.model')
 
 router.route('/').get((req, res) => {
@@ -6,24 +7,43 @@ router.route('/').get((req, res) => {
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json('Error: ' + err))
 })
-
-router.route('/add').post((req, res) => {
+router.route('/login').get((req, res) => {
+  const email = req.body.email
+  User.find({ email: email }, { password: 1, _id: 0 })
+    .then(async (passwordRaw) => {
+      const hashedPassword = passwordRaw[0].password
+      const requestedPassword = req.body.password
+      try {
+        isAllowed = await bcrypt.compare(requestedPassword, hashedPassword)
+        if (isAllowed) res.send('Allowed')
+        else res.send('Not Allowed')
+      } catch {
+        res.sendStatus(500)
+      }
+    })
+    .catch((err) => res.status(400).json('User not found'))
+})
+router.route('/signup').post(async (req, res) => {
   const email = req.body.email
   const firstName = req.body.firstName
   const lastName = req.body.lastName
   const password = req.body.password
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: hashedPassword,
+    })
 
-  const newUser = new User({
-    email: email,
-    firstName: firstName,
-    lastName: lastName,
-    password: password,
-  })
-
-  newUser
-    .save()
-    .then(() => res.json('User added!'))
-    .catch((err) => res.status(400).json('Error: ' + err))
+    newUser
+      .save()
+      .then(() => res.json('User added!'))
+      .catch((err) => res.status(400).json('Error: ' + err))
+  } catch {
+    res.status(500).send
+  }
 })
 
 module.exports = router
