567273e50df7d1900aa66501c6341a27e70a0f63
Vallisha M <vallishamohan@gmail.com>
2021-05-29 08:16:24 +0530

    jwt auth complete

--- backend/authServer.js
+++ backend/authServer.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
+
 const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 let User = require('./models/user.model')
 const mongoose = require('mongoose')
-
+let Refresh = require('./models/refresh_token.model')
 const app = express()
 
 app.use(express.json())
 const port = process.env.PORT || 4000
 const uri = process.env.ATLAS_URI
-let refreshTokens = []
+
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -26,6 +27,25 @@ app.get('/', (req, res) => {
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json('Error: ' + err))
 })
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  Refresh.find({ token: refreshToken }, { _id: 0 })
+    .then(
+      jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, user) => {
+          if (err) return res.sendStatus(403)
+          const accessToken = generateAccessToken({ name: user.name })
+          res.json({ accessToken: accessToken })
+        }
+      )
+    )
+    .catch(() => {
+      return res.sendStatus(400)
+    })
+})
 app.post('/login', (req, res) => {
   const email = req.body.email
   User.find({ email: email }, { password: 1, _id: 0 })
@@ -34,14 +54,29 @@ app.post('/login', (req, res) => {
       const requestedPassword = req.body.password
       try {
         isAllowed = await bcrypt.compare(requestedPassword, hashedPassword)
-        if (isAllowed) res.send('Allowed')
-        else res.send('Not Allowed')
+        if (isAllowed) {
+          const user = { email: email }
+
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '10m',
+          })
+
+          const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+          const newRefreshToken = new Refresh({ token: refreshToken })
+          newRefreshToken
+            .save()
+            .then(() =>
+              res.json({ accessToken: accessToken, refreshToken: refreshToken })
+            )
+            .catch((err) => res.status(400).json('Error: ' + err))
+        } else res.status(403).send('Not Allowed')
       } catch {
         res.sendStatus(500)
       }
     })
     .catch((err) => res.status(400).json('User not found'))
 })
+
 app.post('/signup', async (req, res) => {
   const email = req.body.email
   const firstName = req.body.firstName
@@ -64,7 +99,18 @@ app.post('/signup', async (req, res) => {
     res.sendStatus(500)
   }
 })
-
+app.delete('/logout', (req, res) => {
+  if (req.body.token) {
+    tokens = Refresh.find({ token: req.body.token }, { _id: 0 })
+    if (tokens) {
+      Refresh.remove({ token: req.body.token })
+        .then(() => res.json('Logged Out'))
+        .catch(() => res.json('ERROR'))
+    }
+    res.status(403).json('Not Logged in')
+  }
+  res.status(400).json('Bad request')
+})
 app.listen(port, () => {
   console.log(`Authorization Server is running on port: ${port}`)
 })
